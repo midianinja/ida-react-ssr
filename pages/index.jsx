5601d02f37fe628fabfd239bd13110cd49753a2d
Rodrigo G. Oliveira <mjgargani@gmail.com>
2020-07-29 21:03:56 -0300

    feat(logo): adicionado botao como um component de img com alt(acessibilidade)

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,13 @@
-import Body from "../src/components/template/body/Body"
-import Modal from "../src/components/organisms/modal/Modal"
-import Title from "../src/components/atoms/title/Title"
-import Flex from "../src/components/molecules/flex/Flex"
-import Logo from "../src/components/atoms/logo/Logo"
+import React from 'react';
+import Body from '../src/components/template/body/Body';
+import Modal from '../src/components/organisms/modal/Modal';
+import Logo from '../src/components/atoms/logo/Logo';
 
-export default (props) => {
-  return (
-    <Body>
-      <Modal header={<Logo scale={0.7}/>}>
-        <Flex column>
-          <Flex>
-            <Title>Faça seu login!</Title>
-          </Flex>
-          <Flex>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </Flex>
-        </Flex>
-      </Modal>
-    </Body>
-  )
-}
+export default () => (
+  <Body>
+    <Modal header={
+      <Logo width={184} height={65} scale={0.7} logo="ida/ida_white" altText="Logotipo da IDA" />
+    }
+    />
+  </Body>
+);
